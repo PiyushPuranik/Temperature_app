3d97c93e7c4c7d9c8b88ca828af14edb4444bfb9
PiyushPuranik <102739662+PiyushPuranik@users.noreply.github.com>
2024-04-02 10:18:48 +0530

    Add files via upload
    Weather App

---
+++ App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import TempApp from './components/TempApp';
+import React from 'react';
+
+function App() {
+  return (
+    <TempApp/>
+  );
+}
+
+export default App;
